8bf8549650001211d1be68fe1e577553fd84e055
Fernando Alvarado <fernandoalvarado@ciencias.unam.mx>
2025-11-16 17:13:31 -0600

    server2

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,11 +1,50 @@
 library(shiny)
+library(ggplot2)
 
 ui <- fluidPage(
-  h2("Hola desde VS Code!")
+  titlePanel("Ejemplo parámetros"),
+
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("dist", "Distribución:",
+                  choices = c("Normal", "Uniforme", "Exponencial")),
+      sliderInput("n", "n:", min = 50, max = 2000, value = 500, step = 50),
+      sliderInput("bins", "Número de bins:", min = 5, max = 50, value = 30)
+    ),
+
+    mainPanel(
+      plotOutput("hist"),
+      verbatimTextOutput("summary")
+    )
+  )
 )
 
-server <- function(input, output, session){}
+server <- function(input, output, session) {
+
+  # 1) Objeto reactivo: se recalcula cuando cambian sus inputs
+  datos <- reactive({
+    n <- input$n
+
+    if (input$dist == "Normal") {
+      rnorm(n)
+    } else if (input$dist == "Uniforme") {
+      runif(n)
+    } else {
+      rexp(n)
+    }
+  })
+
+  # 2) Gráfica
+  output$hist <- renderPlot({
+    x <- datos()
+    ggplot(data.frame(x), aes(x)) +
+      geom_histogram(bins = input$bins)
+  })
+
+  # 3) Resumen numérico
+  output$summary <- renderPrint({
+    summary(datos())
+  })
+}
 
 shinyApp(ui, server)
-
-#"C:/Users/ferna/Documents/Proyectos_Diplomado/Renewable-Energy/ShinyApp"
